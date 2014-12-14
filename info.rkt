a91bb31b7772e32e1f97dae5cfd15bd04a423f40
Leif Andersen <leif@leifandersen.net>
2014-12-14 13:05:21 -0700

    Switch to multi collection.

--- info.rkt
+++ info.rkt
@@ -1,14 +1,14 @@
 #lang info
+
 (define collection "line-editor")
+
 (define deps '("base"
                "rackunit-lib"
                "cext-lib"))
 (define build-deps '("scribble-lib"
                      "racket-doc"))
+
 (define scribblings '(("scribblings/line-editor.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(leif))
-
-(define pre-install-collection "private/install.rkt")
-(define compile-omit-files '("private/install.rkt"))
---
+++ line-editor/info.rkt
@@ -0,0 +1,6 @@
+#lang info
+
+(define scribblings '(("scribblings/line-editor.scrbl" ())))
+
+(define pre-install-collection "private/install.rkt")
+(define compile-omit-files '("private/install.rkt"))
