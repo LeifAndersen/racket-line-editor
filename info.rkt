9d5b43c89fe0288dbb6fe595967a6f09bd837957
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-12-15 01:36:35 -0500

    Remove reference to scribblings at package level

--- info.rkt
+++ info.rkt
@@ -8,7 +8,6 @@
 (define build-deps '("scribble-lib"
                      "racket-doc"))
 
-(define scribblings '(("scribblings/line-editor.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(leif))
